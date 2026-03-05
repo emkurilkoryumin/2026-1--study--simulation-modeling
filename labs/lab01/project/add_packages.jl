7dafc7b2ac55c25e6f4e68f8c42b8bc3b8e4250a
Eugene Kurilko-Ryumin <1132232883@pfur.ru>
2026-03-06 01:40:58 +0300

    feat: add lab01 and lab02 without nested git repos

--- labs/lab01/project/add_packages.jl
+++ labs/lab01/project/add_packages.jl
@@ -1,21 +1,25 @@
-##!/usr/bin/env julia
-## add_packages.jl
+#!/usr/bin/env julia
+# add_packages.jl
 using Pkg
-Pkg.activate(".") # Активируем текущий проект
-## ОСНОВНЫЕ ПАКЕТЫ ДЛЯ РАБОТЫ
+
+# Активируем текущий проект
+Pkg.activate(".")
+
+# Список пакетов для установки
 packages = [
-"DrWatson", # Организация проекта
-"DifferentialEquations", # Решение ОДУ
-"Plots", # Визуализация
-"DataFrames", # Таблицы данных
-"CSV", # Работа с CSV
-"JLD2", # Сохранение данных
-"Literate", # Literate programming
-"IJulia", # Jupyter notebook
-"BenchmarkTools", # Бенчмаркинг
-"Quarto" # Создание отчетов 
+    "DrWatson",
+    "DataFrames",
+    "Plots",
+    "StatsBase",
+    "CSV",
+    "JLD2"
 ]
-println("Установка базовых пакетов...")
-Pkg.add(packages)
-println("\n✅ Все пакеты установлены!")
-println("Для проверки: using DrWatson, DifferentialEquations, Plots")
+
+# Устанавливаем пакеты
+for pkg in packages
+    println("Устанавливаю $pkg...")
+    Pkg.add(pkg)
+end
+
+println("✅ Все пакеты установлены!")
+Pkg.status()
--- labs/lab01/setup_project.jl
+++ labs/lab01/setup_project.jl
@@ -1,13 +1,10 @@
 ##!/usr/bin/env julia
 ## setup_project.jl
-
-
 using Pkg
 Pkg.add("DrWatson")
 using DrWatson
 ## Создание проекта
 project_name = "project"
-initialize_project(project_name; authors="Ваше Имя", git=false)
-
-println(" Проект создан: ", project_name)
-println("� Перейдите в директорию: cd ", project_name)  
+initialize_project(project_name; authors="Eugene Kurilko-Ryumin", git=false)
+println("✅ Проект создан: ", project_name)
+println("� Перейдите в директорию: cd ", project_name)
